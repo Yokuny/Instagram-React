effd3c815f2cb1add8a15d87a18e95135a11eb94
yokuny <felipe.vni@hotmail.com>
2023-02-10 17:32:13 -0300

    Updates

---
+++ functions.js
@@ -0,0 +1,46 @@
+function salvar(id) {
+  const bookmarkFeito = document.createElement("img");
+  bookmarkFeito.src = "./assets/bookmark-svgrepo-com.svg";
+  const bookmarkVazio = document.getElementById(id);
+  bookmarkVazio.innerHTML = "";
+  bookmarkVazio.append(bookmarkFeito);
+}
+function comentar(id) {
+  const userNameH4 = document.createElement("h4");
+  userNameH4.textContent = "iso.profile";
+
+  const userComentP = document.createElement("p");
+  userComentP.textContent = document.getElementById(id).value;
+  document.getElementById(id).value = "";
+
+  const heartIcon = document.createElement("div");
+  heartIcon.innerHTML = '<ion-icon name="heart-outline"></ion-icon>';
+
+  const justDiv = document.createElement("div");
+  justDiv.appendChild(userNameH4);
+  justDiv.appendChild(userComentP);
+
+  const divForTheNewElements = document.createElement("div");
+  divForTheNewElements.classList.add("theComment");
+  divForTheNewElements.appendChild(justDiv);
+  divForTheNewElements.appendChild(heartIcon);
+
+  const divThatHoldEverything = document.createElement("div");
+  divThatHoldEverything.classList.add("newComment");
+  divThatHoldEverything.appendChild(divForTheNewElements);
+  document.getElementById(`comentField-id-${id}`).appendChild(divThatHoldEverything);
+  /*
+        <div id="comentField-id-rrbbh">
+    
+            <div id="comentField-id-rrbbh">
+    
+                <div class="newComment">
+                    <div class="theComment">
+                        <div>
+                            <h4>yokuny</h4><p>What's your method for sharing the project with the client?</p>
+                        </div>
+                        <ion-icon name="heart-outline"></ion-icon>
+                    </div>
+                </div>
+    */
+}
